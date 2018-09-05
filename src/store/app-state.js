a76742c312684d7e3a865cc7b0ead73dde076515
zhuxiuwu <1453855454@qq.com>
2018-09-05 12:00:15 +0800

    init

---
+++ src/store/app-state.js
@@ -0,0 +1,24 @@
+import {observable,computed,autorun,action} from 'mobx';
+
+class AppState {
+    @observable count = 0;
+    @observable name = 'Jokcy';
+    @computed get msg(){
+        return `${this.name} say  count is ${this.count}`
+    }
+    @action add(){
+        this.count +=1
+    }
+}
+
+const appState = new AppState();
+
+autorun (()=>{
+    console.log(appState.msg)
+});
+
+setInterval((()=>{
+   appState.add();
+}));
+
+export default appState
